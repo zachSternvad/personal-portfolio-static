c34d77bca50a41db388dc4e68ce9b235f9b147d2
Zachery <zachery.meck@gmail.com>
2025-05-29 21:30:50 +0200

    Started the JavaScript

---
+++ myscript.js
@@ -0,0 +1,8 @@
+const themeToggleButton = document.getElementById("theme-toggle");
+const bodyElement = document.body;
+const sunIcon = themeToggleButton.querySelector(".fa-sun");
+const moonIcon = themeToggleButton.querySelector(".fa-moon");
+
+themeToggleButton.addEventListener("click", function () {
+    console.log("Knapp Klicketh");
+})
