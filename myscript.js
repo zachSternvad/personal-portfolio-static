2c2a807ee28e8c6894e5e6e48708e0ee770f9c95
Zachery <zachery.meck@gmail.com>
2025-05-29 21:40:19 +0200

    Added the theme toggle icon function

--- myscript.js
+++ myscript.js
@@ -4,5 +4,14 @@ const sunIcon = themeToggleButton.querySelector(".fa-sun");
 const moonIcon = themeToggleButton.querySelector(".fa-moon");
 
 themeToggleButton.addEventListener("click", function () {
-    console.log("Knapp Klicketh");
-})
+    bodyElement.classList.toggle("dark-mode"); // Växla klassen
+
+    // Växla ikoner
+    if (bodyElement.classList.contains("dark-mode")) {
+        moonIcon.style.display = "inline-block";
+        sunIcon.style.display = "none";
+    } else {
+        moonIcon.style.display = "none";
+        sunIcon.style.display = "inline-block";
+    }
+});
